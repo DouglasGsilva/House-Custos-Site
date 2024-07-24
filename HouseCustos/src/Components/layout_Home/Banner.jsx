d1e9f4b31b17927ef1df6e6517dc674f0f917d26
DouglasGsilva <goDouglasdev@gmail.com>
2024-07-24 12:11:28 -0300

    Mudança grande no layout

--- HouseCustos/src/Components/layout_Home/Banner.jsx
+++ HouseCustos/src/Components/layout_Home/Banner.jsx
@@ -1,21 +1,22 @@
-import React from "react";
-import "./Banner.css"
-import { Link } from "react-router-dom"
+import "./Banner.css";
+import { Link } from "react-router-dom";
 function Banner() {
-
-
-
-    return (
-        <header>
-            <div className="banner">
-                <div className="img">
-                    <h1>House Custos</h1>
-                    <p>Um site onde contará com o custo da minha casa, ultilizando uma api e
-                        podendo fazer o calculo das despesas, fazendo assim uma administração mais concisa.</p>
-                    <Link className="btn" to="/custos">Vamos Checar</Link>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="img">
+        <div className="banner">
+          <h1>House Custos</h1>
+          <p>
+            Um site onde contará com o custo da minha casa, ultilizando uma api
+            e podendo fazer o calculo das despesas, fazendo assim uma
+            administração mais concisa.
+          </p>
+          <Link className="btn" to="/custos">
+            Vamos Checar
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
-export default Banner
+export default Banner;
--- HouseCustos/src/Components/layout_Home/Integrantes.jsx
+++ HouseCustos/src/Components/layout_Home/Integrantes.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import image1 from "../imgs/homemadulto.png"
-import image2 from "../imgs/painho.png"
-import image3 from "../imgs/mainha.png"
-import { Carousel } from "antd"
-import "./Integrantes.css"
-const imagens = [image1, image2, image3]
-
+import image1 from "../imgs/homemadulto.png";
+import image2 from "../imgs/painho.png";
+import image3 from "../imgs/mainha.png";
+import { Carousel } from "antd";
+import "./Integrantes.css";
+const imagens = [image1, image2, image3];
 
 function Integrantes() {
   return (
     <>
-      <h2 id='h2'>Integrantes da Casa</h2>
-      <Carousel autoplay="true" className='carrousel' >
-        <div className='content'>
-          <img className='real_img' alt='imagem 1' src={imagens[0]} />
+      <h2 id="h2">Integrantes da Casa</h2>
+      <Carousel autoplay="true" className="carrousel">
+        <div className="content">
+          <img className="real_img" alt="imagem 1" src={imagens[0]} />
           <h2>Integrante 1</h2>
-          <h3 className='ocupation'>Ocupação</h3>
-          <div className='review'>Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+          <h3 className="ocupation">Ocupação</h3>
+          <div className="review">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </div>
         </div>
         <div>
-          <img className='real_img' alt='imagem2' src={imagens[1]} />
-          <h2 >Integrante 2</h2>
-          <h3 className='ocupation'>Ocupação</h3>
-          <div className='review'>Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+          <img className="real_img" alt="imagem2" src={imagens[1]} />
+          <h2>Integrante 2</h2>
+          <h3 className="ocupation">Ocupação</h3>
+          <div className="review">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </div>
         </div>
         <div>
-          <img className='real_img' alt='imagem3' src={imagens[2]} />
+          <img className="real_img" alt="imagem3" src={imagens[2]} />
           <h2>Integrante 3</h2>
-          <h3 className='ocupation'>Ocupação</h3>
-          <div className='review'>Lorem ipsum dolor sit amet consectetur adipisicing.</div>
-          <p>
-
-          </p>
+          <h3 className="ocupation">Ocupação</h3>
+          <div className="review">
+            Lorem ipsum dolor sit amet consectetur adipisicing.
+          </div>
+          <p></p>
         </div>
-
       </Carousel>
     </>
   );
 }
-
 
 export default Integrantes;
