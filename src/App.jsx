7d83d32453455b3e0efb705bc3668fb6f2953adf
DouglasGsilva <goDouglasdev@gmail.com>
2024-09-23 18:29:03 -0300

    layout da page cadastro feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Fragment } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/pages/Home/Home";
--- src/Components/pages/Cadastro/Cadastro.jsx
+++ src/Components/pages/Cadastro/Cadastro.jsx
@@ -1,5 +1,35 @@
+import "../Cadastro/Cadastro.css";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 const Cadastro = () => {
-  return <div>cadastro page ok</div>;
+  const {
+    register: registerSignup,
+    handleSubmit: handleSubmitSignup,
+    formState: { errors: errorsSignup },
+  } = useForm({});
+
+  function inHandleSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <div className='container-main'>
+      <h1 className='logo'>House Custos</h1>
+      <form className='input-box' onSubmit={handleSubmitSignup(inHandleSubmit)}>
+        <p className='placeholder'>Digite seu nome:</p>
+        <input className='input-data' type='text' register={registerSignup} />
+        <p className='placeholder'>Digite seu email:</p>
+        <input className='input-data' type='email' register={registerSignup} />
+        <p className='placeholder'>Digite sua senha:</p>
+        <input
+          type='password'
+          className='input-data'
+          register={registerSignup}
+        />
+        <button className='btn-cadastrar'>Cadastrar</button>
+      </form>
+    </div>
+  );
 };
 
 export default Cadastro;
