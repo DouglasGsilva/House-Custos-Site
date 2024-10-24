661f7649df7faf770cc47008ca3ea7364332d14b
DouglasGsilva <goDouglasdev@gmail.com>
2024-10-24 18:56:56 -0300

    tudo certo agora

--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -4,8 +4,9 @@ import MoreInfo from "../../layout_Home/MoreInfo/MoreInfo.jsx";
 import About from "../../layout_Home/About/About.jsx";
 import Footer from "../../layout_Home/Footer/Footer.jsx";
 import NavBar from "../../layout_Home/Navbar/Navbar.jsx";
-import { useEffect } from "react";
+
 import { ContainerHome } from "./style.jsx";
+import Cookies from "js-cookie";
 
 function Home() {
   return (
