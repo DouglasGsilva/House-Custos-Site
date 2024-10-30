2079c98d3925c52651f2957ea4ddf8027fa601e9
DouglasGsilva <goDouglasdev@gmail.com>
2024-10-30 18:53:35 -0300

    mobile first feito

--- HouseCustos/src/Components/layout_Home/About/About.jsx
+++ HouseCustos/src/Components/layout_Home/About/About.jsx
@@ -12,34 +12,6 @@ function About() {
         </C.Text>
         <C.Button>Testar</C.Button>
         <C.Img src={ImageWoman} />
-        <C.ContentCircle>
-          <C.Circle
-            color='#3d3ddd'
-            width='35px'
-            widthL='40px'
-            height='35px'
-            heightL='40px'
-            left='65px'
-          />
-          <C.Circle
-            color='#2881ca'
-            width='25px'
-            widthL='30px'
-            height='25px'
-            heightL='30px'
-            left='55px'
-            bot='35px'
-          />
-          <C.Circle
-            color='#888686'
-            width='20px'
-            widthL='25px'
-            height='20px'
-            heightL='25px'
-            left='35px'
-            bot='15px'
-          />
-        </C.ContentCircle>
       </C.AboutContainer>
     </>
   );
