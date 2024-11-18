1aa91d16dde7e9603ae2863f86c0ee41e5bfa899
DouglasGsilva <goDouglasdev@gmail.com>
2024-11-18 10:59:23 -0300

    adição da api renderizada

--- HouseCustos/src/Components/pages/Custos/CustosComponents/TableItem/style.js
+++ HouseCustos/src/Components/pages/Custos/CustosComponents/TableItem/style.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const ContainerColumn = styled.div`
   display: flex;
   justify-content: center;
-  background-color: yellow;
 `;
 
 export const TableColumn = styled.td`
